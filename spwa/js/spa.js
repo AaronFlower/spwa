fc302b4eff25bda3f2419aa7198e928bef5b222d
easonzhan <easonzhan@tencent.com>
2015-11-14 21:58:40 +0800

    Update code chapter 3

---
+++ spwa/js/spa.js
@@ -0,0 +1,13 @@
+/**
+* spa.js
+* Root namespace module
+*/
+
+/*global $, spa */
+
+var spa = ( function(){
+	var initModule = function( $container ){
+		spa.shell.initModule( $container );
+	}
+	return { initModule : initModule };
+} )();
